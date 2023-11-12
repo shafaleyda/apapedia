159b9eaff7504ef62e1da9437a1123b54c690bae
Rangga Yudhistira <ranggabbrata@gmail.com>
2023-11-12 22:49:26 +0700

    rangga/feat/user/#3 & #2

---
+++ user/src/main/java/com/apapedia/user/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.apapedia.user.user;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+  Optional<User> findByEmail(String email);
+
+}
