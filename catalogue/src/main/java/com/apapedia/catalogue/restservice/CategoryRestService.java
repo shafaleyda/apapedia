66662893bebb3d1c8414101e0e2606e282244cec
Ramya Nareswari Wikantiyoso <ramya.nareswari@ui.ac.id>
2023-11-12 23:33:02 +0700

    Ramya

--- catalogue/src/main/java/com/apapedia/catalogue/restservice/CategoryRestService.java
+++ catalogue/src/main/java/com/apapedia/catalogue/restservice/CategoryRestService.java
@@ -1,9 +1,9 @@
 package com.apapedia.catalogue.restservice;
 
 import com.apapedia.catalogue.model.Category;
-import java.util.List; 
+import java.util.List;
 
 public interface CategoryRestService {
-    void saveCategory(Category category); 
-    List<Category> retrieveAllCategory(); 
+    void saveCategory(Category category);
+    List<Category> retrieveAllCategory();
 }
