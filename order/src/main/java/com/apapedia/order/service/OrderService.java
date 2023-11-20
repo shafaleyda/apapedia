213979a76264d79f372ba0c55a23a27d91d985b1
Nadhif Rahman Alfan <nadhif.rahman11@ui.ac.id>
2023-11-20 19:30:26 +0700

    Revert "Merge branch 'main' into 'nadhif'"
    This reverts merge request !15

--- order/src/main/java/com/apapedia/order/service/OrderService.java
+++ order/src/main/java/com/apapedia/order/service/OrderService.java
@@ -1,12 +1,20 @@
 package com.apapedia.order.service;
 
 import com.apapedia.order.model.OrderModel;
+
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderService {
-    OrderModel createOrder(OrderModel order);
+    void saveOrder(OrderModel order);
+    
+    List<OrderModel> retrieveAllOrder();
 
-    OrderModel getOrderById(UUID id);
+    OrderModel getOrderByOrderId(UUID id);
 
-    OrderModel updateOrder(OrderModel order);
+    OrderModel updateOrder(OrderModel order, OrderModel oldOrder);
+
+    List<OrderModel> listByCustomer(UUID customer);
+
+    List<OrderModel> listBySeller(UUID seller);
 }
