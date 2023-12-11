94a23e1ac05823b3bdbdb09ae8672aea9cf758f0
Rangga Yudhistira Brata <rangga.yudhistira@ui.ac.id>
2023-12-11 20:54:20 +0700

    Rangga/feat/user/fe mobile #1

--- user/src/main/java/com/apapedia/user/restcontroller/AuthenticationRestController.java
+++ user/src/main/java/com/apapedia/user/restcontroller/AuthenticationRestController.java
@@ -1,6 +1,7 @@
 package com.apapedia.user.restcontroller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.apapedia.user.service.AuthenticationService;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/authentication")
 @RequiredArgsConstructor
